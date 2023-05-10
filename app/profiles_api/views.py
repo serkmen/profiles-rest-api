f18d34c45aa8831747869aabba9edcd53121e770
Serkan Erkmen <serkan.erkmen@ericsson.com>
2023-05-10 21:59:54 +0300

    APIView class implemented, URL pattern added

---
+++ app/profiles_api/views.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLS',
+        ]
+
+        # HTTP function must return a reponse
+        # Ressponse in the format of JSON in either list or dictionary format
+        return Response({'message': 'Hello', 'an_apiview':an_apiview})
+
